d1f2cc050a634acb3afbc0254aec623cb9ae97ae
Ohenewaah09 <aoetsorblewu@st.ug.edu.gh>
2024-05-24 16:08:08 +0000

    increase of font size

--- SplashZone/App.js
+++ SplashZone/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Adaiah Ohenewaah Edinam Tsorblewu</Text>
+      <Text style={styles.text}>My name is Adaiah Ohenewaah Edinam Tsorblewu</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,6 +17,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
    
+  },
+  text:{
+    fontSize: '24',
   },
  
 });
